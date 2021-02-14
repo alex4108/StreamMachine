21f778d493388fba013f8c15ad934ac346e72b5c
Alex Schittko <alex4108@live.com>
2021-02-14 03:34:10 -0600

    Fix push

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@ docker build -t alex4108/streammachine:nms .
 cd ../web
 docker build -t alex4108/streammachine:web .
 if [[ "${DEV}" != "1" ]]; then
-	docker push
+	docker push alex4108/streammachine:nms
+	docker push alex4108/streammachine:web
 fi
